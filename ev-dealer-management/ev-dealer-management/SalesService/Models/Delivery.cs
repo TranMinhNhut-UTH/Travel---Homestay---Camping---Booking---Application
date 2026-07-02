ce59f95d65e7a74a015d568ec09584c6d8e33e38
tmtienn <tientrinh050205@gmail.com>
2026-07-03 00:14:08 +0700

    ED-32: Commit local fixes for Payments/Deliveries and migrations

--- ev-dealer-management/ev-dealer-management/SalesService/Models/Delivery.cs
+++ ev-dealer-management/ev-dealer-management/SalesService/Models/Delivery.cs
@@ -16,7 +16,7 @@ namespace SalesService.Models
         public Guid DeliveryId { get; set; }
 
         // FK
-        public Guid OrderId { get; set; }
+        public int OrderId { get; set; }
         public Order? Order { get; set; }
 
         public DateTime? DeliveryDate { get; set; }
